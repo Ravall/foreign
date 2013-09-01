cebd1c3b9a3f162ed7314280e79ca0dd80ee0fbd
Ravall <valery.ravall@gmail.com>
2013-09-01 23:21:12 +0400

    allowed host

--- frgn/frgn/settings.py
+++ frgn/frgn/settings.py
@@ -29,7 +29,7 @@ else:
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['engdel.ru']
 
 
 TIME_ZONE = 'Europe/Moscow'
