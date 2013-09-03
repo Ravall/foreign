7f544d8a46180ff060b30e6849c84ffa8efc6b19
Ravall <valery.ravall@gmail.com>
2013-09-03 23:13:14 +0400

    DAISY SCHOOL MOSCOW

--- frgn/frontend/views.py
+++ frgn/frontend/views.py
@@ -75,6 +75,20 @@ def psi(request):
         context_instance=RequestContext(request)
     )
 
+def school(request, article_name):
+    data = _get_tag_articles('school')
+    content = utils.api_request(
+        'engdel/article/{0}.json'.format(article_name)
+    )
+    if not content:
+        raise Http404
+    content = json.loads(content)
+    data['about_content'] = content
+    return render_to_response(
+        'frontend/school.html',
+        data,
+        context_instance=RequestContext(request)
+    )
 
 def index(request):
     # теоретические материалы
